bfcc4914fc3df4f4278d2556b97b47107642e486
Vladimir Simin <vladimir.simin@gmail.com>
2021-10-07 20:16:14 +0200

    first task

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>{{$post->title}}</title>
+</head>
+
+<body>
+    <a href="/posts">Go to posts</a>
+    <h2>
+        {{$post->title}}
+    </h2>
+    <p>
+        {{$post->body}}
+    </p>
+</body>
+
+</html>
